d2eec65b48ce53b1c4f72f764a89970bfd0d64e7
7yultukuri7 <49602287+7yultukuri7@users.noreply.github.com>
2019-04-14 14:33:08 +0900

    Update main.js
    log

--- docs/main.js
+++ docs/main.js
@@ -4,13 +4,14 @@ $(function(){
         var username = $('#username').val();
         var content = $('#content').val();
         var sty1 = 'color:#40e0d0;background:#484b52;';
-        console.log("%c Link:      " + "%s \n" + " Username: " + " %s \n" + " content:  " + " %s \n", sty1, link, username, content);
+        console.log("%c Webhook Link:      " + "%s \n" + " Bot Username: " + " %s \n" + " content:  " + " %s \n", sty1, link, username, content);
         if (link==null || link=="",content==null || content=="")
         {
             alert("すべての欄に記入してください");
             return false;
         }
         $.post(link, {"content": content, "username": username});
+        $('#log').append('<br><span>Webhook Link:' + link + '<br>Bot Username:' + username + '<br>content:' + content + '</span><br>');
         console.log("コンテンツ送信完了!");
     });
 });
